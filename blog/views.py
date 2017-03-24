ab34524dd74d19dc2524068fe296cf4545335535
tmttknmpfpm <tmttknmpfpm@163.com>
2017-03-24 17:06:13 +0800

    *

--- blog/views.py
+++ blog/views.py
@@ -3,7 +3,6 @@ from .models import Post
 # Create your views here.
 def post_list(request):
     posts = Post.objects.all()
-    colors = [
-        'red', 'orange', 'yellow','green'
-    ]
-    return render(request, 'blog/post_list.html', {'posts':posts, 'colors':colors})
+    # colors = '0 1 2 3'
+    # c2 = '10 12 14 55'
+    return render(request, 'blog/post_list.html', {'posts':posts})
--- mysite/settings.py
+++ mysite/settings.py
@@ -34,6 +34,7 @@ ALLOWED_HOSTS = [
 # Application definition
 
 INSTALLED_APPS = [
+    'bootstrap_admin',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -62,14 +63,20 @@ TEMPLATES = [
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
-                'django.template.context_processors.debug',
-                'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
+                'django.template.context_processors.debug',
+                'django.template.context_processors.i18n',
+                'django.template.context_processors.media',
+                'django.template.context_processors.static',
+                'django.template.context_processors.tz',
                 'django.contrib.messages.context_processors.messages',
+                'django.template.context_processors.request'
             ],
         },
     },
 ]
+
+BOOTSTRAP_ADMIN_SIDEBAR_MENU = True
 
 WSGI_APPLICATION = 'mysite.wsgi.application'
 
